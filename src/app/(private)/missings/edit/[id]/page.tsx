5d3bff03cd1dfb25817ac6cefe273295861e6a66
henaorth16 <emyayehenok@gmail.com>
2025-10-28 23:28:00 +0300

    feat: add edit page for the user

---
+++ src/app/(private)/missings/edit/[id]/page.tsx
@@ -0,0 +1,37 @@
+import MissingPersonForm from "@/components/forms/MissingPersonForm";
+import { prisma } from "@/lib/prisma";
+import { MissingPersonFormValues } from "@/lib/validation-schemas";
+import React from "react";
+
+export default async function editpage(params: { id: string }) {
+  const { id } = params;
+  const person = await prisma.missingPerson.findFirst({
+    where: { id },
+  });
+
+  if (!person) {
+    return <div>No data found</div>;
+  }
+  const initialData: MissingPersonFormValues = {
+    id: person.id,
+    fullName: person.fullName,
+    gender: person.gender,
+    dob: person.dob.toISOString().split("T")[0],
+    lastSeenDate: person.lastSeenDate.toISOString().split("T")[0],
+    lastSeenPlace: person.lastSeenPlace,
+    clothing: person.clothing ?? "",
+    physicalMarks: person.physicalMarks ?? "",
+    description: person.description ?? "",
+    reward: person.reward?.toString() ?? "",
+    photoUrl: person.photoUrl ?? "",
+  };
+
+  if (!initialData) {
+    return <div>No record found for this person.</div>;
+  }
+  return (
+    <div className="space-y-4">
+      <MissingPersonForm initialData={initialData} />
+    </div>
+  );
+}
--- src/app/(private)/missings/new/page.tsx
+++ src/app/(private)/missings/new/page.tsx
@@ -3,39 +3,10 @@ import { prisma } from "@/lib/prisma";
 import { MissingPersonFormValues } from "@/lib/validation-schemas";
 import React from "react";
 
-
-
-
-
-export default async function editpage() {
-   const person = await prisma.missingPerson.findFirst({
-    where: { fullName: "Henok emyaye" },
-  });
-
-  if (!person) {
-    return <div>No data found</div>;
-  }
-
-  const initialData: MissingPersonFormValues = {
-      id: person.id,
-      fullName: person.fullName,
-      gender: person.gender,
-      dob: person.dob.toISOString().split('T')[0],
-      lastSeenDate: person.lastSeenDate.toISOString().split('T')[0],
-      lastSeenPlace: person.lastSeenPlace,
-      clothing: person.clothing ?? '',
-      physicalMarks: person.physicalMarks ?? '',
-      description: person.description ?? '',
-      reward: person.reward?.toString() ?? '',
-      photoUrl: person.photoUrl ?? '',
-    };
-
-  if (!initialData) {
-    return <div>No record found for this person.</div>;
-  }
+export default async function page() {
   return (
     <div className="space-y-4">
-      <MissingPersonForm initialData={initialData} />
+      <MissingPersonForm />
     </div>
   );
 }
